eec978bfa6951d2902a74cd16638150284b0e05c
Hendi Santika <hendisantika@gmail.com>
2022-04-13 19:41:56 +0700

    #24 - Add addResourceHandlers & filter in WebMvcConfiguration

--- src/main/java/com/hendisantika/config/WebMvcConfiguration.java
+++ src/main/java/com/hendisantika/config/WebMvcConfiguration.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,4 +23,25 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/", "classpath:/resources/",
             "classpath:/static/", "classpath:/public/"};
+
+    /**
+     * Main method to configure possible locations.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**") .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    /**
+     * Registering the application HTTP filter
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean filter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new ResetPassDemoFilter());
+        bean.addUrlPatterns("/*");  // or use setUrlPatterns()
+
+        return bean;
+    }
 }

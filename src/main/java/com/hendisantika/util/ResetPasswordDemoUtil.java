86122291d063f66ee72712ffee546c217733be17
Hendi Santika <hendisantika@gmail.com>
2022-04-13 19:43:39 +0700

    #26 - Add encryptSHY2 in ResetPasswordDemoUtil

---
+++ src/main/java/com/hendisantika/util/ResetPasswordDemoUtil.java
@@ -0,0 +1,45 @@
+package com.hendisantika.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/04/22
+ * Time: 19.42
+ */
+@Slf4j
+public abstract class ResetPasswordDemoUtil {
+    // ===============SHY2 method ====================
+    /**
+     * Method will encrypt a given string with SHY2 algorithm
+     * @param password
+     * @return {@link String}
+     */
+    public static String encryptSHY2(String password) {
+        MessageDigest md;
+        StringBuffer hexString = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            byte[] byteData = md.digest();
+
+            hexString = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                String hex = Integer.toHexString(0xff & byteData[i]);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hexString.toString();
+    }
+}

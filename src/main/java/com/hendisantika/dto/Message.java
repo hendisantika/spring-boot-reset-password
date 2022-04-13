fff65877e0d295eac59b5458e7fe5ecacd359771
Hendi Santika <hendisantika@gmail.com>
2022-04-13 19:39:15 +0700

    #22 - Add Message DTO

---
+++ src/main/java/com/hendisantika/dto/Message.java
@@ -0,0 +1,21 @@
+package com.hendisantika.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/04/22
+ * Time: 19.38
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    public String message;
+}

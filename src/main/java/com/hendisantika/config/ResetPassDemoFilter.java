6f0c13ba1fe55cc0b684f4f2a2a110b25ff353a0
Hendi Santika <hendisantika@gmail.com>
2022-04-13 21:07:23 +0700

    #31 - Add ResetPassDemoFilter

---
+++ src/main/java/com/hendisantika/config/ResetPassDemoFilter.java
@@ -0,0 +1,20 @@
+package com.hendisantika.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/04/22
+ * Time: 21.06
+ */
+@Component
+public class ResetPassDemoFilter implements Filter {
+
+    private static final String USER = "user";
+}
